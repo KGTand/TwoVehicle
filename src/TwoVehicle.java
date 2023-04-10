7e247075921182222c97c0191d88f484da2ee00a
KGTand <129801318+KGTand@users.noreply.github.com>
2023-04-10 23:25:51 +0300

    Простые методы и классы 3 машины и поезд

--- src/TwoVehicle.java
+++ src/TwoVehicle.java
@@ -26,14 +26,17 @@ public class TwoVehicle {
        Vehicle car1 = new Vehicle(); 
        Vehicle car2 = new Vehicle();
        Vehicle car3 = new Vehicle();
+       Vehicle car4 = new Vehicle();
     
        car1.set(4, 200, 100, "minivan");
        car2.set(2, 250, 300, "sportscar");
        car3.set(10, 500, 500, "bus");
+       car4.set(50, 1000, 1500, "train");
     
        car1.text();
        car2.text();
        car3.text();
+       car4.text();
 
     }
 }
